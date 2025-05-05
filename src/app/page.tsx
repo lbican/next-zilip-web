1bb1eec69ac5f6ebdda07d17f7d787df3914415f
Luka Bićan <lbican@Mac.home>
2025-05-05 21:44:26 +0200

    Removed chakra components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
-import { ColorModeButton } from "@/components/ui/core/color-mode";
-import { Button } from "@chakra-ui/react";
 
 export default function Page() {
   return (
     <>
-      <Button>TEST</Button>
-      <ColorModeButton />
+      <p>hello</p>
     </>
   );
 }
